2a2ba8f052a389644365e952997bed12fec02e02
ericcheww <ericche@ww.com>
2020-07-30 00:42:32 -0400

    planet queries ii

--- graph/49_plants_queries_ii.cpp
+++ graph/49_plants_queries_ii.cpp
@@ -15,110 +15,74 @@ typedef vector<string> vs;
 typedef long long ll;typedef unsigned int unit32;typedef unsigned long long ull;typedef vector<ll> vi;typedef vector<vector<ll> > vvi;typedef map<ll, ll> mii;typedef map<string, string> mss;typedef map<ll, string> mis;typedef map<string, ll> msi;typedef pair<ll, ll> pii;
 
 const ll maxn = 2e5 + 1;
+ll n,q;
 ll vis[maxn];
-ll adj[maxn];
+vi adj[maxn];
 ll rt[maxn];
-ll root[maxn];
-ll cycpos[maxn];
-ll tocyc[maxn];
-ll chain[maxn];
-ll cycsize[maxn];
-ll n;
-
-ll dfs(ll u, ll r) {
-  if(rt[u]) return -2;
-  if(vis[u]) {
-    rt[u] = r;
-    cycpos[u] = 1; return 1;
+ll d = 1;
+ll ds[maxn], de[maxn], cp[maxn], t[maxn], cs[maxn], cycpos[maxn], dt[maxn];
+void dfs(ll u) {
+  ds[u] = d++;
+  for(ll v : adj[u]) {
+    if(rt[v] != v) {
+      dt[v] = dt[u] + 1;
+      rt[v] = rt[u];
+      cp[v] = cp[u];
+      dfs(v);
+    }
   }
-  vis[u] = 1;
-  ll pos = dfs(adj[u], r) + 1;
-  if(pos < 0) return -2;
-  if(cycpos[u]) {
-    cycsize[r] = pos - 2;
-    return -2;
-  }
-  cycpos[u] = pos;
-  rt[u] = r;
-  return pos + 1;
+  de[u] = d++;
 }
 
-
-
-pair<ll, ll> dfs2(ll u, ll r) {
-  if(rt[u]) {
-    root[rt[u]] = r;
-    return {u, 0};
-  }
-  root[u] = r;
-  auto res = dfs2(adj[u], r);
-  tocyc[u] = res.first;
-  chain[u] = res.second + 1;
-  return {res.first, res.second + 1};
-}
-
+ll p[maxn];
 int main() {
   IOS
-  ll q; cin>>n>>q;
-  vi in(n + 1);
-  for(int i = 1; i <= n; ++i) {
-    cin>>adj[i];
-    ++in[adj[i]];
+  cin>>n>>q;
+  for(ll i = 1; i <= n; ++i) {
+    cin>>t[i];
+    adj[t[i]].push_back(i);
   }
+  memset(rt, -1, sizeof(rt));
   for(int i = 1; i <= n; ++i) {
-    if(!vis[i])
-      dfs(i, i);
-  }
-  memset(vis, 0, sizeof(vis));
-  for(int i = 1; i <= n; ++i) {
-    if(in[i] == 0) {
-      dfs2(i, i);
+    if (rt[i] == -1) {
+      ll u = i;
+      vi cyc;
+      
+      while(!vis[u]) {
+        vis[u] = u;
+        p[u] = t[u];
+        u = t[u];
+      }
+      while(!cyc.size() || u != cyc[0]) {
+        cycpos[u] = cyc.size();
+        cyc.push_back(u);
+        rt[u] = u;
+        u = t[u];
+      }
+
+      for(ll ci : cyc) {
+        cs[ci] = cyc.size();
+        cp[ci] = i;
+        dfs(ci);
+      }
     }
   }
-  // cout<<"rt: ";
-  // for(ll i = 1; i<=n; ++i) {
-  //   cout<<rt[i]<<" ";
-  // }
-  // cout<<"chain: ";
-  // for(ll i = 1; i<=n; ++i) {
-  //   cout<<chain[i]<<" ";
-  // }
-  // cout<<"cycpos: ";
-  // for(ll i = 1; i<=n; ++i) {
-  //   cout<<cycpos[i]<<" ";
-  // }
-  // cout<<"cycsize: ";
-  // for(ll i = 1; i<=n; ++i) {
-  //   cout<<cycsize[i]<<" ";
-  // }
-  // cout<<endl;
   while(q--) {
     ll a, b; cin>>a>>b;
-    if(root[rt[a] ? rt[a] : a] != root[rt[b] ? rt[b] : b]) {
-      cout<<-1<<endl;
+    if(a == b) {
+      cout<<0<<endl;
       continue;
     }
-    if(rt[a] && !rt[b]) {
-      cout<<-1<<endl;
-      continue;
+    ll ans = -1;
+    if(cp[a] == cp[b]) {
+      if(rt[b] == b) {
+        cout<<dt[a] + ((cs[b] + cycpos[b] - cycpos[rt[a]]) % cs[b])<<endl;
+        continue;
+      }else if(ds[b]< ds[a] && de[a] < de[b]){
+        cout<<dt[a] - dt[b]<<endl;
+        continue;
+      }
     }
-    if(!rt[a] && !rt[b]) { 
-      if(chain[a] < chain[b])
-        cout<<-1<<endl;
-      else
-        cout<<chain[a] - chain[b]<<endl;
-      continue;
-    }
-    // cout<<"final"<<endl;
-    ll dis = 0;
-    ll src = a;
-    if(!rt[a]) {
-      dis = chain[a];
-      a = tocyc[a];
-    }
-    ll size = cycsize[rt[b]];
-    // cout<<size<<" :size  to cyc:"<<tocyc[src]<<endl;
-    cout<<((size + cycpos[b] - cycpos[a]) % size) + dis<<endl;
+    cout<<ans<<endl;
   }
-  return 0;
 }
